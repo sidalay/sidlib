4fcabb392a0b4da1ac7eedced094166311bfa891
Sid Shingaki <sidalaytv@gmail.com>
2022-11-13 20:45:10 -0800

    chore: cleanup

--- src/datastructures/arrays/static_array/array.hpp
+++ src/datastructures/arrays/static_array/array.hpp
@@ -14,7 +14,6 @@ namespace sidlib {
         const T& operator[](unsigned int Index) const {return m_Data[Index];}
         const T* Data()                         const {return m_Data;}
     private:
-        T* m_Iterator;
         T  m_Data[S];
     };
 }

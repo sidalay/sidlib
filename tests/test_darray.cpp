40d289fb67d6e51d6e66c8656bd081733a59f7ac
Sid Shingaki <sidalaytv@gmail.com>
2022-11-17 21:08:45 -0800

    feat: dynamic_array tests
    - impl push_back() test cases for darray

--- tests/test_darray.cpp
+++ tests/test_darray.cpp
@@ -1,5 +1,46 @@
+#include <iostream>
+#include <string>
 #include "dArray.hpp"
 
+template <typename T>
+void print_dynamic(const sidlib::darray<T>& darray) {
+    if (darray.size() == 0) {
+        std::cout << "dynamic array is empty";
+    }
+    for (auto& elem : darray) {
+        std::cout << elem << ' ';
+    }
+    std::cout << "\n---------------------------------------\n";
+}
+
+template <typename T>
+void print_capacity(const sidlib::darray<T>& darray) {
+    std::cout << "data.size()\t: " << darray.size() << '\n';
+    std::cout << "data.capacity()\t: " << darray.capacity() << '\n';
+    std::cout << "---------------------------------------\n";
+}
+
 int main() {
+    sidlib::darray<std::string> data;
+
+    // We can observe the capacity of data expanding
+    print_dynamic(data);
+    print_capacity(data);
+
+    data.push_back("sid");
+    data.push_back("loves");
+    data.push_back("c++");
+
+    print_dynamic(data);
+    print_capacity(data);
+
+    data.push_back("adding");
+    data.push_back("some");
+    data.push_back("more");
+    data.push_back("elements");
+    
+    print_dynamic(data);
+    print_capacity(data);
+    // -----------------------------------------------
 
 }

93c49f0bb900de7d5090007a745e0cb1cf6b17b8
Sid Shingaki <sidalaytv@gmail.com>
2023-02-04 19:03:34 -0800

    feat: initial intrinsics files

---
+++ include/sidlib/utility/intrinsics/intrinsics.hpp
@@ -0,0 +1,31 @@
+#ifndef SIDLIB_INTRINSICS_HPP
+#define SIDLIB_INTRINSICS_HPP
+
+#include <immintrin.h>
+
+namespace sidlib
+{
+    using float_128     = __m128;
+    using int_128       = __m128i;
+    using double_128    = __m128d;
+
+    using float_128u    = __m128_u;
+    using int_128u      = __m128i_u;
+    using double_128u   = __m128d_u;
+
+    using float_256     = __m256;
+    using int_256       = __m256i;
+    using double_256    = __m256d;
+
+    using float_256u    = __m256_u;
+    using int_256u      = __m256i_u;
+    using double_256u   = __m256d_u;
+
+
+    struct simd 
+    {
+
+    };
+}
+
+#endif // SIDLIB_INTRINSICS_HPP
